9021f48caa2ccd05b5b5bb7fda4696238cb47d25
adkl <aalibegovi2@etf.unsa.ba>
2017-05-11 15:57:24 +0200

    not working code removed

--- Oglasi.Client/app/app.js
+++ Oglasi.Client/app/app.js
@@ -1,7 +1,7 @@
 
 var app = angular.module('oglasi', ['ngRoute',/* 'ui.bootstrap'/*, 'angular-loading-bar'*/, 'LocalStorageModule']);
 
-var apiurl = "http://localhost:8080/";
+var apiurl = "http://localhost:8080";
 
 app.constant('ngAuthSettings', {
     apiServiceBaseUri: apiurl,
@@ -15,10 +15,6 @@ app.config(function ($routeProvider) {
     templateUrl: "views/userAccount/login.html"
   });
 
-  $routeProvider.when("/adverts", {
-    controller: "advertsListController",
-    templateUrl: "views/advert/all.html"
-  });
   $routeProvider.when("/account/register", {
     controller: "registerController",
     templateUrl: "views/userAccount/register.html"
@@ -28,22 +24,6 @@ app.config(function ($routeProvider) {
       controller: "userDetailsController",
       templateUrl: "views/userAccount/details.html"
   });
-
-
-  $routeProvider.when("/advert/details", {
-    controller: "advertDetailsController",
-    templateUrl: "views/advert/advert_details.html"
-  });
-
-   $routeProvider.when("/advert/details/inappropriate", {
-    controller: "inappropriateAdvertController",
-    templateUrl: "views/advert/inappropriate_advert.html"
-  });
-
-  //   $routeProvider.when("/advert/details/application_form", {
-  //   controller: "applicationOnAdvertController",
-  //   templateUrl: "views/advert/application_form.html"
-  // });
 
    $routeProvider.when("/advert/:id/subscriptions", {
     controller: "subscriptionsOnAdvertController",
--- Oglasi.Client/app/services/advertService.js
+++ Oglasi.Client/app/services/advertService.js
@@ -7,33 +7,7 @@
 
         var advertServiceFactory = {};
 
-        var _getAdverts = function () {
-
-            return $http.get(servicebase + '/advert/all')
-                        .then(function (results) {
-                            return results;
-                        });
-        };
-
-         var _getAdvert = function () {
-
-            return $http.get(servicebase + '/advert/advert_details')
-                        .then(function(results) {
-                            return results;
-                        });
-        };
-
-
-         var _messageInappropriate = function (newMessage) {
-
-            return $http.post(servicebase + '/advert/inappropriate_advert', newMessage)
-                        .then(function (results) {
-                            return results;
-                        });
-        };
-
-
-        var _getSubscrins = function (id) {
+        var _getSubscriptions = function (id) {
 
             return $http.get(servicebase + '/advert/'+id+'/subscriptions')
                         .then(function(results) {
@@ -41,19 +15,9 @@
                         });
         };
 
-
-
-       
-
-        advertServiceFactory.getAdverts = _getAdverts;
-        advertServiceFactory.getAdvert = _getAdvert;
-        advertServiceFactory.messageInappropriate = _messageInappropriate;
-        advertServiceFactory.subscriptions=_getSubscrins;
-
-
+        advertServiceFactory.subscriptions= _getSubscriptions;
 
         return advertServiceFactory;
     }]);
 
 }());
-
